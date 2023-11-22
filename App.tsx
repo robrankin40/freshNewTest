5d532cc793c58a02a33ceb3b4fb7af0745994c1b
Robert Rankin <rankinrobert40@gmail.com>
2023-11-22 10:39:13 -0700

    Permissionr request and fetch geo location

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import type {PropsWithChildren} from 'react';
-import {FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+} from 'react-native';
+import {PERMISSIONS, check, request} from 'react-native-permissions';
 
 import {
   ApolloClient,
@@ -11,6 +19,7 @@ import {
   useQuery,
   ApolloError,
 } from '@apollo/client';
+import Geolocation, {GeoPosition} from 'react-native-geolocation-service';
 
 const client = new ApolloClient({
   uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
@@ -140,6 +149,43 @@ const StarShipList: React.FC = () => {
 
 function App(): JSX.Element {
   const isHermes = () => !!global.HermesInternal as Boolean;
+  const [hasPermission, setPermission] = useState<boolean>(false);
+  const [location, setLocation] = useState<
+    Geolocation.GeoCoordinates | undefined
+  >();
+
+  const checkPermission = async () => {
+    const result = await check(
+      Platform.OS === 'ios'
+        ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+        : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+    );
+    if (result === 'granted') {
+      setPermission(true);
+    } else {
+      setPermission(false);
+      const requestResult = await request(
+        Platform.OS === 'ios'
+          ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+          : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+      );
+      if (requestResult === 'granted') {
+        setPermission(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    checkPermission();
+  }, []);
+
+  useEffect(() => {
+    if (hasPermission) {
+      Geolocation.getCurrentPosition((position: GeoPosition) => {
+        setLocation(position.coords);
+      });
+    }
+  }, [hasPermission]);
 
   return (
     <ApolloProvider client={client}>
